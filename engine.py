d314cccabe1106505fff7c90988093232617d747
purebleusong <helloworld@fuck.com>
2015-10-31 13:18:41 +0800

    a fucking commit

--- engine.py
+++ engine.py
@@ -5,19 +5,23 @@ qq_group_num = '296386547'
 initmsg = u'Game init... \nVersion:'+version+'\nCreated by HIT.SF\nAll Copyrights Reserved\nWelcome to join us !QQ group:'+qq_group_num
 print initmsg
 import sys,os
+import time
 def cur_file_dir():
-     #获取脚本路径
-     path = sys.path[0]
-     #判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
-     if os.path.isdir(path):
-         return path
-     elif os.path.isfile(path):
-         return os.path.dirname(path)
+    #获取脚本路径
+    path = sys.path[0]
+    #判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
+    if os.path.isdir(path):
+        return path
+    elif os.path.isfile(path):
+        return os.path.dirname(path)
 
 sys.path.append(cur_file_dir())
 from catstory_story import storys
 from catstory_story import variables
-import game	
+import game
+currrent_chapter = 'start'
+save_suffix = '.hit.sf'
+split_symbol = '$$$'
 
 def make_error(msg):
     print msg
@@ -29,7 +33,7 @@ def startStory():
 def isMethod(obj):
     return obj.hasattr(obj, '__call__')
 
-def getOptions(chapter,story):
+def getOptions(chapter, story):
     options = []
     if type(story[chapter][1]) is not list:
         make_error('the type of the options is not list')
@@ -44,7 +48,58 @@ def getOptions(chapter,story):
             make_error('invalid data format.\nit should be a tuple or string\nPlease contact Sprout')
     return options
 
+def save(name='default', timestramp=True):
+    global currrent_chapter
+    try:
+        f = open(name+save_suffix,'a+')
+        f.write(currrent_chapter+(split_symbol+str(int(time.mktime(time.localtime())))) if timestramp else '')
+        f.close()
+    except Exception, e:
+        raise
+        return True
+    else:
+        return False
+
+def get_saves(name='default'):
+    saves = []
+    i = 0
+    try:
+        f = open(name+save_suffix, 'r')
+        records = str(f.read()).split('\n')
+        f.close()
+        for record in records:
+            record = record.split(split_symbol)
+            if len(record) > 1:
+                timestramp = int(record[1])
+                record_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestramp))
+                saves.append((i, record[0], record_time))
+            else:
+                saves.append((i, record))
+            i += 1
+    except Exception, e:
+        make_error('error:Maybe the file does not exit')
+        raise e
+        return saves
+    else:
+        return saves
+
+def load(version=-1, name='default'):
+    global currrent_chapter
+    try:
+        f = open(name+save_suffix, 'r')
+        records = str(f.read()).split('\n')
+        if version==-1 || version>len(records):
+            currrent_chapter = (records[-1]).split('$$$')[0]
+        if len(records)>version:
+            currrent_chapter = (records[version]).split('$$$')[0]
+        f.close()
+    except Exception, e:
+        make_error('error:load-Maybe file does not exist')
+        raise
+    return currrent_chapter
+
 def getnextchapter(chapter='start',story=storys):
+    global currrent_chapter
     options = []
     if len(story[chapter]) == 2:
         options = getOptions(chapter,story)
@@ -59,7 +114,7 @@ def getnextchapter(chapter='start',story=storys):
     nextchapter = indecies[choose]
     if type(nextchapter) == tuple:
         nextchapter = nextchapter[1]
+    currrent_chapter = nextchapter
     return nextchapter
 
 print 'engine init over'
-
--- game.py
+++ game.py
@@ -2,21 +2,55 @@ import sys,os
 sys.path.append("../")#make import the branch possible
 import engine
 from cmd_color_picker import *
+helpinfo = u'''帮助信息：
+输入S保存进度
+输入L载入进度
+'''
+stop_loop_chapter = False
 
 def input(msg):
-	return raw_input(msg)
+	value = raw_input(msg)
+	return value
 
-def output(msg,color=0x0f,suffix='\n'):
-	log(msg,color,suffix)
+def output(msg, color=0x0f, suffix='\n'):
+	log(msg, color, suffix)
+
+def save():
+	engine.save()
+
+def load():
+	output('This is saves:', foreGroundColor.DARKSKYBLUE)
+	saves = engine.get_saves()
+	for save in saves:
+		output(str(save[0]), foreGroundColor.PINK,' ')
+		output(save[1], foreGroundColor.PURPLE, ' ')
+		output(save[2], foreGroundColor.YELLOW)
+	i = int(input('choose your save'))
+	engine.load(i-1)
+
+def inputMsgDispatch(value):
+	if value == 'S':
+		save()
+	elif value == 'L':
+		load()
+	elif value == 'help':
+		output(helpinfo)
+	else:
+		try:
+			value = int(value)
+		except Exception, e:
+			value = None
+			raise e
+		else:
+			return value		
 
 def choose_story(storys):
-	output('Please choose story',foreGroundColor.PURPLE)
-	i=0
+	output('Stories', foreGroundColor.PURPLE)
+	i = 0
 	for story in storys:
-		i+=1
-		output(str(i)+' '+story,foreGroundColor.SKYBLUE)
-	return int(input('Please choose your story'))
-
+		i += 1
+		output(str(i)+' '+story, foreGroundColor.SKYBLUE)
+	return input('Please choose your story')
 
 def choose_chapter(chapters):
 	i=0
@@ -26,20 +60,23 @@ def choose_chapter(chapters):
 		output(str(i),foreGroundColor.DARKRED,':')
 		output(nextbranch,foreGroundColor.YELLOW,' ') 
 	print ' '
-	return int(input('Please choose it!'))
+	return input('Please choose it!')
 
 def show_chapter(chapter):
 	output(chapter,foreGroundColor.PURPLE)
 
+def loop_chapter(chapter):
+	while(chapter is not 'end' && !stop_loop_chapter):# loop the story line
+		show_chapter(story[chapter][0])
+		chapter = engine.getnextchapter(chapter, story)#^-^
+	
 #main
 def main():
 	chapter = 'start' # define start chapter
-	print 'start game:'
+	print 'start game: input \'help\' get help infomation'
 	story = engine.startStory()# choose story
 	print 'OK Come on!'
-	while(chapter is not 'end'):# loop the story line
-		show_chapter(story[chapter][0])
-		chapter = engine.getnextchapter(chapter, story)#^-^
+	loop_chapter(chapter)
 	show_chapter(story['end'][0])
 
 if __name__ == '__main__':
